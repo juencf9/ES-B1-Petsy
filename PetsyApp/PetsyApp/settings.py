c11aef26ab4fd2b6b7490e95671009e9c78568fd
juencf9 <juencf9@hotmail.com>
2019-12-06 19:51:12 +0100

    rework settings

--- PetsyApp/PetsyApp/settings.py
+++ PetsyApp/PetsyApp/settings.py
@@ -51,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'PetsyApp.urls'
